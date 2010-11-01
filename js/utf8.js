03c63b09a635edaf0bb53a2384b031ccd932d5a3
Guillaume Marty <edo999@gmail.com>
2010-11-01 17:48:27 +0100

    Add metadata to encoded file

--- js/utf8.js
+++ js/utf8.js
@@ -1,5 +1,6 @@
 /**
- * UTF-8 data decode
+ * UTF-8 data encode / decode
  * http://www.webtoolkit.info/
  */
-var Utf8={decode:function(c){for(var d="",a=0,b=c1=c2=0;a<c.length;){b=c.charCodeAt(a);if(b<128){d+=String.fromCharCode(b);a++}else if(b>191&&b<224){c2=c.charCodeAt(a+1);d+=String.fromCharCode((b&31)<<6|c2&63);a+=2}else{c2=c.charCodeAt(a+1);c3=c.charCodeAt(a+2);d+=String.fromCharCode((b&15)<<12|(c2&63)<<6|c3&63);a+=3}}return d}};
+var Utf8={encode:function(a){if(!a.length)return"";a=a.replace(/\r\n/g,"\n");for(var b="",d=a.length,c=0;c<d;c++){var e=a.charCodeAt(c);if(e<128)b+=String.fromCharCode(e);else{if(e>127&&e<2048)b+=String.fromCharCode(e>>6|192);else{b+=String.fromCharCode(e>>12|224);b+=String.fromCharCode(e>>6&63|128)}b+=String.fromCharCode(e&63|128)}}return b},decode:function(a){if(!a.length)return"";for(var b="",d=0,c=c1=c2=0,e=a.length;d<e;){c=a.charCodeAt(d);if(c<128){b+=String.fromCharCode(c);d++}else if(c>191&&
+c<224){c2=a.charCodeAt(d+1);b+=String.fromCharCode((c&31)<<6|c2&63);d+=2}else{c2=a.charCodeAt(d+1);c3=a.charCodeAt(d+2);b+=String.fromCharCode((c&15)<<12|(c2&63)<<6|c3&63);d+=3}}return b}};
